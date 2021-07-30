72f9eadea3e4e7d1127e8035a40154d1d32bd4d5
Tony Bowden <tony@tmtm.com>
2021-07-30 18:54:23 +0300

    wikibase-cli creation scripts

---
+++ create-office.js
@@ -0,0 +1,23 @@
+// wd create-entity create-office.js "Minister for X"
+module.exports = (label) => {
+  return {
+    type: 'item',
+    labels: {
+      en: label,
+    },
+    descriptions: {
+      en: 'government position in Greenland',
+    },
+    claims: {
+      P31:   { value: 'Q294414' }, // instance of: public office
+      P279:  { value: 'Q83307'  }, // subclas of: minister
+      P1001: { value: 'Q223'    }, // jurisdiction: Greenland
+      P361: {
+        value: 'Q10858355',         // part of: Government of Greenland
+        references: {
+          P854: 'https://naalakkersuisut.gl/en/Naalakkersuisut/Medlemmer-af-Naalakkersuisut',
+        },
+      }
+    }
+  }
+}
